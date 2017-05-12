edcd7b5b5a712655c0ac15fe53e25a109ee36fbd
Indri Muska <indrimuska@gmail.com>
2017-05-12 20:28:37 +0200

    Update images and credits

--- script.js
+++ script.js
@@ -3,31 +3,31 @@ var columns = [ 12, 6, 6, 3, 3, 3, 3, 6, 6, 12 ],
 	images = [
 		{
 			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-8-all/13',
-			source:  'http://yourshot.nationalgeographic.com/u/ss/fQYSUbVfts-T7Ehj8_lPSALDyndvMSNW9paJ9-zKyhc9WOHwTz_HJZV1PUjKLmZxxfsKxzFlKTfmZvJDie7bugKZJhz0j4GfrgatGr1FIhSZBX_3kPYK3nG_dcxSuosO98066GwetCM2h6dHQzncc5rh_NLVATYunSKedq1kLzVM/'
+			source:  'img/img-1.jpg'
 		},
 		{
-			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-8-all/11',
-			source:  'http://yourshot.nationalgeographic.com/u/ss/fQYSUbVfts-T7Ehj8_lPSALDyndvMSNW9paJ9-zKyhc9WOHwTz_HJZV1PUIym2rifL_mWvf_0OP76EIeSqiLi523-B5MQvV8ZHDLW5aQw3gOB8zLTJs7qHQJnIsLc-YIGQJnmB7gPoE72O9xsx2ldwOoXmmO7h_d6ibwQc-2Ux7K/'
-		},
-		{
-			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-6-all/13',
-			source:  'http://yourshot.nationalgeographic.com/u/ss/fQYSUbVfts-T7Ehj8_lPSALDyndvMSNW9paJ9-zKyhc9WOHwTz_HJZV1N7I2yFTKer28tO-NP2QIBkDTClyjlBRxbUx2YgeCilgcW181kE1Dre3iV-KZA346JotWVtNlwiW0-DSiWwYCY7LN0ZMz7HJXFRRAfL_-CxUoEwyu0gfC/'
-		},
-		{
-			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-1-all/10',
-			source:  'http://yourshot.nationalgeographic.com/u/ss/fQYSUbVfts-T7Ehj8_lPSALDyndvMSNW9paJ9-zKyhc9WOHwTz_HJZRKlOx6kP2CCEjshDbOApSVRhHgmuqFQLaP-yUM1IE93-UpY8i0xAi1nRLgYA5ZB0aTW2aXoTzoX4Cx02dOj9YFDBsKCNi7deZ12lWdiAlwEEMv6nynQ8Rd/'
-		},
-		{
-			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-2-all/3',
-			source:  'http://yourshot.nationalgeographic.com/u/ss/fQYSUbVfts-T7Ehj8_lPSALDyndvMSNW9paJ9-zKyhc9WOHwTz_HJZc3j2ugq-ge9Gp3AlN05UvpDsni9gWY5pzEy1FlRzS2gf1jLrYeSTCKCkP7zzQytwjvqu4GyMKTr5DR0a1069XA8dN0nyGQXj8D3zFCRQLROzUxsPgFgUQv/'
-		},
-		{
-			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-5-all/11',
-			source:  'http://yourshot.nationalgeographic.com/u/ss/fQYSUbVfts-T7Ehj8_lPSALDyndvMSNW9paJ9-zKyhc9WOHwTz_HJZRE2ttwc4NHS5G_brP8lAQ_F1T-yUObfnh2GCzVZwgIGVyB-uNuvgUj7Qj-4vlvB9V1COizQFEicrpDCWNwULc2fxfiimKqk0NUg9jQcPJuAckB8LTKyJhw/'
+			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-10-all/17',
+			source:  'img/img-2.jpg'
 		},
 		{
 			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-6-all/7',
-			source:  'http://yourshot.nationalgeographic.com/u/ss/fQYSUbVfts-T7Ehj8_lPSALDyndvMSNW9paJ9-zKyhc9WOHwTz_HJZRNVThfFHVaVYjsbK2TtGGcoSLGkPVYSqCCMbPdMGWkT2rlpV9vhr16avHILtc7Hc8JuZZZa1x33XLKmpwfXIA6NiQlGnoiai6LxJLT04fyYJYhCtt5Kd3q/'
+			source:  'img/img-3.jpg'
+		},
+		{
+			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-11-all/13',
+			source:  'img/img-4.jpg'
+		},
+		{
+			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-8-all/16',
+			source:  'img/img-5.jpg'
+		},
+		{
+			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-5-all/11',
+			source:  'img/img-6.jpg'
+		},
+		{
+			credits: 'http://photography.nationalgeographic.com/contest-2015/wallpapers/week-11-all/26',
+			source:  'img/img-7.jpg'
 		}
 	];
 
